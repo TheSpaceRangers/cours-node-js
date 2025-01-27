8da0ee809eabc7123b075e60fcd1acb32cd94005
TheSpaceRangers <tronel.charles@gmail.com>
2025-01-27 14:37:21 +0100

    Add route to fetch a task by its ID

--- gestion-taches-redis-mysql/routes/tasks.js
+++ gestion-taches-redis-mysql/routes/tasks.js
@@ -4,6 +4,7 @@ const tasksController = require('../controllers/tasksController');
 
 // Routes
 router.get('/', tasksController.getTasks);
+router.get('/:id', tasksController.getTaskById);
 router.post('/', tasksController.createTask);
 router.put('/:id', tasksController.updateTask);
 router.delete('/:id', tasksController.deleteTask);
